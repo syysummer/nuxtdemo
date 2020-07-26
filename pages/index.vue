48c80f95203f50cc10b97facedf61c4effc35dd5
sunyangyang <syysummer@yeah.net>
2020-07-26 16:01:07 +0800

    to reset

---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        我的Nuxt项目
+      </h1>
+      <div class="links">
+      <el-rate
+        v-model="value2"
+        :colors="colors">
+      </el-rate>
+      <el-button type="primary" :loading="true">加载中</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+        value2: null,
+        colors: ['#99A9BF', '#F7BA2A', '#FF9900']  // 等同于 { 2: '#99A9BF', 4: { value: '#F7BA2A', excluded: true }, 5: '#FF9900' }
+      }
+    }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 30px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ plugins/ElementUI.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+import { Button, Rate } from 'element-ui' //引入分页组件
+
+Vue.use(Button);
+Vue.use(Rate);
+
+// Vue.prototype.$message = Message;
